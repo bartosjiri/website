6a61ca32ecbcd6bf0e706e872b991296eef67c6a
bartosjiri <me@bartosjiri.com>
2024-01-16 19:51:07 +0100

    Update tooltip

--- src/lib/primitives/Tooltip.svelte
+++ src/lib/primitives/Tooltip.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { fade } from 'svelte/transition';
+	import { slide, fade } from 'svelte/transition';
+	import { sineInOut } from 'svelte/easing';
 
 	let content: string | null;
 	let isHovering = false;
@@ -28,10 +29,14 @@
 <svelte:window on:mousemove={handleMouseMove} />
 <div
 	class:tooltip-tracker={true}
-	style={`top: ${cursorPosition[1] + 4}px; left: ${cursorPosition[0] + 4}px;`}
+	style={`top: ${cursorPosition[1] + 12}px; left: ${cursorPosition[0] + 12}px;`}
 >
 	{#if isHovering && content}
-		<div class:tooltip={true} transition:fade={{ duration: 150 }}>
+		<div
+			class:tooltip={true}
+			in:slide={{ duration: 125, easing: sineInOut, axis: 'x' }}
+			out:fade={{ duration: 125, easing: sineInOut }}
+		>
 			<span>{content}</span>
 		</div>
 	{/if}
@@ -43,7 +48,8 @@
 
 		.tooltip {
 			display: none;
-			padding: var(--spacing-01) var(--spacing-03);
+			position: relative;
+			width: 100%;
 			background: var(--color-background-01);
 			border: 1px solid var(--color-foreground-02);
 			border-radius: 10px;
@@ -52,12 +58,14 @@
 			z-index: 9999;
 
 			span {
+				display: block;
 				font-size: var(--font-size-01);
+				padding: var(--spacing-01) var(--spacing-03);
 				white-space: pre;
 			}
 
 			@media (hover: hover) {
-				display: initial;
+				display: block;
 			}
 		}
 	}
